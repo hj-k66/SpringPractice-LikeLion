2b9e3cc7022a94581f2ff9421a69c18f1a0f4318
heejeong <hjx66@naver.com>
2022-10-21 11:30:50 +0900

    UserDao 테스트~try/catch/finally 처리까지 복습 1. deleteAll(), .getCount()추가 – 166p 2. deleteAll(), .getCount() 테스트 추가 3. @BeforeEach 적용하여 테스트코드 개선 - 182p 4. add()메소드에서 user == null인 경우 Exception처리 - 175p 5. try / catch / finally 처리 - 213p

--- src/main/java/com/lion/dao/UserDao.java
+++ src/main/java/com/lion/dao/UserDao.java
@@ -6,6 +6,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import org.springframework.dao.EmptyResultDataAccessException;
 
 public class UserDao {
     private ConnectionMaker connectionMaker;
@@ -16,6 +17,79 @@ public class UserDao {
 
     public UserDao(ConnectionMaker connectionMaker) {
         this.connectionMaker = connectionMaker;
+    }
+
+    public void deleteAll() throws SQLException {
+        Connection c = null;
+        PreparedStatement ps = null;
+        try {
+            c = connectionMaker.makeConnection();
+
+            ps = c.prepareStatement("DELETE from users");
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            if(ps != null){
+                try {
+                    ps.close();
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            if(c != null){
+                try {
+                    c.close();
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+
+
+
+    }
+
+    public int getCount() throws SQLException {
+        Connection c = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        int count = 0;
+        try {
+            c = connectionMaker.makeConnection();
+
+            ps = c.prepareStatement("SELECT count(*) from users");
+
+            rs = ps.executeQuery();
+            rs.next();
+            count = rs.getInt(1);
+            return count;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            if(rs != null){
+                try {
+                    rs.close();
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            if(ps != null){
+                try {
+                    ps.close();
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            if(c != null){
+                try {
+                    c.close();
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+
     }
 
     public void add(User user) throws SQLException, ClassNotFoundException {
@@ -45,14 +119,19 @@ public class UserDao {
 
         // Query문 실행
         ResultSet rs = pstmt.executeQuery();
-
-        rs.next();
-        User user = new User(rs.getString("id"), rs.getString("name"),
-                rs.getString("password"));
+        User user = null;
+        if(rs.next()){
+            user = new User(rs.getString("id"), rs.getString("name"),
+                    rs.getString("password"));
+        }
 
         rs.close();
         pstmt.close();
         c.close();
+
+        if(user==null){
+            throw new EmptyResultDataAccessException(1);
+        }
 
         return user;
 
--- src/test/java/com/lion/dao/UserDaoTest.java
+++ src/test/java/com/lion/dao/UserDaoTest.java
@@ -2,6 +2,7 @@ package com.lion.dao;
 
 import com.lion.domain.User;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,16 +18,45 @@ import static org.junit.jupiter.api.Assertions.*;
 class UserDaoTest {
     @Autowired
     ApplicationContext context;
+    UserDao userDao;
+    User user1;
+    User user2;
+    User user3;
+
+
+    @BeforeEach
+    void setUp(){
+        userDao = context.getBean("awsUserDao", UserDao.class);
+        user1 = new User("1","김희정","asdf");
+        user2 = new User("2","김희정","123");
+        user3 = new User("3","김희정","qwer");
+    }
     @Test
     void addAndSelect() throws SQLException, ClassNotFoundException {
-//        UserDao userDao = new UserDao(new AwsConnectionMaker());
-        UserDao userDao = context.getBean("awsUserDao", UserDao.class);
-        String id = "16";
-        User user = new User(id,"do","okokok");
-        userDao.add(user);
+        User user = new User("0","김희정","apapap");
 
-        User selectedUser = userDao.findById(id);
-        Assertions.assertEquals("do", selectedUser.getName());
+        userDao.deleteAll();
+        Assertions.assertEquals(0,userDao.getCount());
+
+
+        userDao.add(user);
+        Assertions.assertEquals(1,userDao.getCount());
+
+        User selectedUser = userDao.findById(user.getId());
+        Assertions.assertEquals(user.getName(), selectedUser.getName());
+    }
+
+    @Test
+    void count() throws SQLException, ClassNotFoundException {
+        userDao.deleteAll();
+        Assertions.assertEquals(0,userDao.getCount());
+
+        userDao.add(user1);
+        userDao.add(user2);
+        userDao.add(user3);
+
+        Assertions.assertEquals(3,userDao.getCount());
+
     }
 
 }
