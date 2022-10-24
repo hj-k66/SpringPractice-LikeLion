d290e37f81d70324dac4151dceedd83f262302fe
heejeong <hjx66@naver.com>
2022-10-24 11:13:59 +0900

    [refactor] AddStrategy 구현 및 적용 1. AddStrategy 구현 2. UserDao의 add()에 적용

--- src/main/java/com/lion/dao/UserDao.java
+++ src/main/java/com/lion/dao/UserDao.java
@@ -95,18 +95,8 @@ public class UserDao {
 
     public void add(User user) throws SQLException, ClassNotFoundException {
 
-        Connection c = connectionMaker.makeConnection();
-
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        PreparedStatement ps = c.prepareStatement("INSERT INTO users(id,name,password) values(?,?,?)"); //sql문 템플릿
-        ps.setString(1,user.getId());
-        ps.setString(2,user.getName());
-        ps.setString(3,user.getPassword());
-
-        ps.executeUpdate(); //Mysql workbench에서 ctrl + enter와 유사
-        //connection끊기
-        ps.close();
-        c.close();
+        StatementStrategy st = new AddStrategy(user);
+        jdbcContextWithStatementStrategy(st);
 
     }
 
